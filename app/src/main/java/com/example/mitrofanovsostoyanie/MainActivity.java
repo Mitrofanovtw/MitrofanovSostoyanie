261d7a47b2512fd7b33c4ea3a756035172a9107f
Mitrofanovtw <mirmirnekitmir25@gmail.com>
2025-10-21 16:20:50 +0300

    Добавлена пз 15 Работа с файловой                 системой и состояниями

--- app/src/main/java/com/example/mitrofanovsostoyanie/MainActivity.java
+++ app/src/main/java/com/example/mitrofanovsostoyanie/MainActivity.java
@@ -7,47 +7,97 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-    User user = new User("Undefined", 0);
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+public class MainActivity extends AppCompatActivity
+{
+    User user = new User("Undefined", 0, "");
     final static String userVariableKey = "USER_VARIABLE";
+    final static String FILE_NAME = "user.data";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        if (savedInstanceState != null) {
+        if (savedInstanceState != null)
+        {
             user = (User) savedInstanceState.getSerializable(userVariableKey);
             TextView dataView = findViewById(R.id.dataView);
-            dataView.setText("Name: " + user.getName() + " Age: " + user.getAge());
+            dataView.setText("Name: " + user.getName() + " Age: " + user.getAge() + " Email: " + user.getEmail());
+        }
+
+        else
+        {
+            getData(null);
         }
     }
 
     @Override
-    protected void onSaveInstanceState(Bundle outState) {
+    protected void onSaveInstanceState(Bundle outState)
+    {
         outState.putSerializable(userVariableKey, user);
         super.onSaveInstanceState(outState);
     }
 
-    public void saveData(View view) {
+    public void saveData(View view)
+    {
         EditText nameBox = findViewById(R.id.nameBox);
         String name = nameBox.getText().toString();
 
         int age = 0;
-        try {
+
+        try
+        {
             EditText yearBox = findViewById(R.id.yearBox);
             age = Integer.parseInt(yearBox.getText().toString());
-        } catch (NumberFormatException ex) {
         }
 
-        user = new User(name, age);
+        catch (NumberFormatException ex)
+        {
+
+        }
+
+        EditText emailBox = findViewById(R.id.emailBox);
+        String email = emailBox.getText().toString();
+
+        user = new User(name, age, email);
 
         TextView dataView = findViewById(R.id.dataView);
-        dataView.setText("Name: " + user.getName() + " Age: " + user.getAge());
+        dataView.setText("Name: " + user.getName() + " Age: " + user.getAge() + " Email: " + user.getEmail());
+
+        try
+        {
+            FileOutputStream fos = openFileOutput(FILE_NAME, MODE_PRIVATE);
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+            oos.writeObject(user);
+            oos.close();
+        }
+
+        catch (Exception ex)
+        {
+
+        }
     }
 
-    public void getData(View view) {
+    public void getData(View view)
+    {
+        try
+        {
+            FileInputStream fin = openFileInput(FILE_NAME);
+            ObjectInputStream ois = new ObjectInputStream(fin);
+            user = (User) ois.readObject();
+            ois.close();
+        }
+        catch (Exception ex)
+        {
+
+        }
+
         TextView dataView = findViewById(R.id.dataView);
-        dataView.setText("Name: " + user.getName() + " Age: " + user.getAge());
+        dataView.setText("Name: " + user.getName() + " Age: " + user.getAge() + " Email: " + user.getEmail());
     }
 }
--- app/src/main/java/com/example/mitrofanovsostoyanie/User.java
+++ app/src/main/java/com/example/mitrofanovsostoyanie/User.java
@@ -1,28 +1,47 @@
 package com.example.mitrofanovsostoyanie;
 
 import java.io.Serializable;
-public class User implements Serializable {
+
+public class User implements Serializable
+{
     private String name;
     private int age;
+    private String email;
 
-    public User(String name, int age) {
+    public User(String name, int age, String email)
+    {
         this.name = name;
         this.age = age;
+        this.email = email;
     }
 
-    public String getName() {
+    public String getName()
+    {
         return name;
     }
 
-    public void setName(String name) {
+    public void setName(String name)
+    {
         this.name = name;
     }
 
-    public int getAge() {
+    public int getAge()
+    {
         return age;
     }
 
-    public void setAge(int age) {
+    public void setAge(int age)
+    {
         this.age = age;
+    }
+
+    public String getEmail()
+    {
+        return email;
+    }
+
+    public void setEmail(String email)
+    {
+        this.email = email;
     }
 }
